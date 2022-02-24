adafaade771b800e6580c8d2759cb9b651aa8055
Matej Capkovic <mcapkovic@gmail.com>
2022-02-24 06:25:20 +0100

    add board preview

---
+++ src/components/EndGameModal/BoardPreview.svelte
@@ -0,0 +1,43 @@
+<script>
+  import { KEY_STATE } from "../../utils/constants";
+
+  export let boardState = [];
+</script>
+
+<div class="preview">
+  {#each boardState as { tilesState }}
+    {#each tilesState as keyState}
+      <div
+        class="tile"
+        class:correct={keyState === KEY_STATE.CORRECT}
+        class:present={keyState === KEY_STATE.PRESENT}
+        class:absent={keyState === KEY_STATE.ABSENT}
+      />
+    {/each}
+  {/each}
+</div>
+
+<style>
+  .preview {
+    display: grid;
+    grid-template-columns: repeat(5, 3rem);
+    grid-gap: 0.2rem;
+  }
+
+  .tile {
+    border: 2px solid #e7e7e7;
+    height: 3rem;
+  }
+
+  .correct {
+    background-color: var(--correct-color);
+  }
+
+  .present {
+    background-color: var(--present-color);
+  }
+
+  .absent {
+    background-color: var(--absent-color);
+  }
+</style>
--- src/components/EndGameModal/WinModal.svelte
+++ src/components/EndGameModal/WinModal.svelte
@@ -2,30 +2,46 @@
   import Modal from "../Modal.svelte";
   import { generateBoardPreview, saveToClipboard } from "../../utils/common";
   import { gameStore } from "../../store";
+  import BoardPreview from "./BoardPreview.svelte";
 
   export let closeModal = () => {};
   export let gameReset = () => {};
 
   const { word, boardState = [] } = $gameStore;
   const sharableBoard = generateBoardPreview(boardState);
-  const textToShare = `${word} ${boardState.length}/6\n${sharableBoard}`;
+  const title = `${word} ${boardState.length}/6`;
+  const textToShare = `${title}\n${sharableBoard}`;
 </script>
 
 <Modal on:close={closeModal}>
   <h2 slot="header">Vyhral si! :)</h2>
-  <pre>{textToShare}</pre>
+
+  <div class="content">
+    <p>{title}</p>
+    <BoardPreview {boardState} />
+  </div>
 
   <svelte:fragment slot="footer">
     <button class="footer-button reset-button" on:click={gameReset}
       >Hraj znova</button
     >
-    <button class="footer-button share-button" on:click={saveToClipboard}
-      >Zdieľať</button
+    <button
+      class="footer-button share-button"
+      on:click={() => saveToClipboard(textToShare)}>Zdieľať</button
     >
   </svelte:fragment>
 </Modal>
 
 <style>
+  .content {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .content p {
+    margin-top: 0;
+  }
   .footer-button {
     color: white;
     border: none;
@@ -37,10 +53,5 @@
 
   .share-button {
     background-color: var(--correct-color);
-  }
-
-  pre {
-    text-align: center;
-    font-size: 25px;
   }
 </style>
